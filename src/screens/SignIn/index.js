258613351b04592ab8916cb1017f2bee097607b9
Felipe Campos <felipec165@gmail.com>
2021-11-13 14:50:18 -0300

    layout tela de login

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { 
+    Container ,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+} from './styles';
+import BarberLogo from '../../assets/barber.svg'
+import EmailIcon from '../../assets/email.svg'
+import LockIcon from '../../assets/lock.svg'
+import SignUp from '../SignUp';
+import SignInput from '../../components/SignInput';
 
 export default () => {
+    
+    const [emailField, setEmailField] = useState('');
+    const [passwordField, setPasswordField] = useState('');
+
     return(
         <Container>
-            <Text>SignIn</Text>
+            <BarberLogo
+                width="100%" height="150"
+            ></BarberLogo>
+            <InputArea>
+
+                <SignInput 
+                    IconSvg={EmailIcon}
+                    placeholder="Email"
+                    value={emailField}
+                    onChangeText={t=>setEmailField(t)}
+                />
+                <SignInput 
+                    IconSvg={LockIcon}
+                    placeholder="Senha"
+                    value={passwordField}
+                    onChangeText={t=>setPasswordField(t)}
+                    password={true}
+                />
+
+                <CustomButton>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignMessageButton>
+                <SignMessageButtonText>Ainda não tem conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>CADASTRE-SE</SignMessageButtonTextBold>
+            </SignMessageButton>
+            
         </Container>
     );
 }
--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -1,4 +1,45 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+    background-color: #050A30;
+    flex: 1;
+    justify-content: center;
+    align-itens: center;
+`;
+
+export const InputArea = styled.View`
+    padding: 40px;
+`;
+
+export const CustomButton = styled.TouchableOpacity`
+    height: 60px;
+    background-color: #0E86D4;
+    border-radius: 30px;
+    justify-content: center;
+    align-itens: center;
+    width: 50%;
+    margin-left: 25%;
+`;
+export const CustomButtonText = styled.Text`
+    font-size: 18px;
+    color: #FFF;
+    textAlign: center;
+`;
+
+export const SignMessageButton = styled.TouchableOpacity`
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 50px;
+    margin-botton: 30px;
+`;
+export const SignMessageButtonText = styled.Text`
+    font-size: 18px;
+    color: #0E86D4;
+`;
+export const SignMessageButtonTextBold = styled.Text`
+    font-size: 18px;
+    color: #0E86D4;
+    font-weight: bold;
+    margin-left: 5px;
+`;
