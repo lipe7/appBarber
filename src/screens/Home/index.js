afa43da54954c4edcdd06cb2a9bde282e5f0b915
Felipe Campos <felipec165@gmail.com>
2021-12-05 22:16:15 -0300

    adicionado loading na home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, {useState} from 'react';
+import { Platform } from 'react-native';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
+
 import { 
     Container,
     Scroller,
@@ -7,12 +10,37 @@ import {
     HeaderTitle,
     LocationArea,
     LocationInput,
-    LocationFinder
+    LocationFinder,
+
+    LoadingIcon
  } from './styles';
 
 import MyLocationIcon from '../../assets/my_location.svg';
 
 export default () => {
+
+    const [locationText, setLocationText] = useState('');
+    const [coords, setCoords] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleLocationFinder = async () => {
+        setCoords(null);
+
+        let result =  await request(
+            Platform.OS === 'ios' ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+        );
+
+        if(result == 'granted'){
+            setLoading(true);
+            Geolocation.getCurrentPosition((info) => {
+                setCoords(info.cords);
+                setLocationText('');
+                setList([]);
+            });
+        }
+    }
+
+
     return (
         <Container>
             <Scroller>
@@ -23,11 +51,16 @@ export default () => {
                 <LocationArea>
                     <LocationInput
                         placeholder="Onde você está?"
-                        placeholderTextColor="#FFFFFF"/>
-                    <LocationFinder >
+                        placeholderTextColor="#FFFFFF"
+                        value={locationText}
+                        onChangeText={t=>setLocationText(t)}
+                        />
+                    <LocationFinder onPress={handleLocationFinder}>
                         <MyLocationIcon width="24" height="24" fill="#FFFFFF" />
                     </LocationFinder>
                 </LocationArea>
+
+                <LoadingIcon size='large' color='#FFF'/>
 
             </Scroller>
         </Container>
