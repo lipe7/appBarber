f3b9c79bde326246bef592bb863e1532dcc2cfba
Felipe Campos <felipec165@gmail.com>
2021-12-05 21:54:37 -0300

    adicionado o input do gps

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Container } from './styles';
+import { 
+    Container,
+    Scroller,
+    HeaderArea,
+    HeaderTitle,
+    LocationArea,
+    LocationInput,
+    LocationFinder
+ } from './styles';
+
+import MyLocationIcon from '../../assets/my_location.svg';
 
 export default () => {
     return (
         <Container>
-            <Text>Home</Text>
+            <Scroller>
+                <HeaderArea>
+                    <HeaderTitle numberOfLines={2}>Encontre um barbeiro</HeaderTitle>
+                </HeaderArea>
+
+                <LocationArea>
+                    <LocationInput
+                        placeholder="Onde você está?"
+                        placeholderTextColor="#FFFFFF"/>
+                    <LocationFinder >
+                        <MyLocationIcon width="24" height="24" fill="#FFFFFF" />
+                    </LocationFinder>
+                </LocationArea>
+
+            </Scroller>
         </Container>
     );
 }
